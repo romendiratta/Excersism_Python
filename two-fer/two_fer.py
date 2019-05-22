99629dc667141ba7c5f13d3b750c1031a911a274
Rohan Mendiratta <romendiratta@gmail.com>
2019-05-22 16:12:03 -0500

    Condensed two-fer code

--- two-fer/two_fer.py
+++ two-fer/two_fer.py
@@ -1,5 +1,2 @@
-def two_fer(name):
-    if type(name) == None:
-        return "One for you, one for me."
-    else:
-        return f"One for {name}, one for me."
+def two_fer(name = "you"):
+    return f"One for {name}, one for me."
